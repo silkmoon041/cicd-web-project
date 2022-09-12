f4893bc55964c3cf34b825c85bddf08acd0ccc79
swy <silkmoon041@gmail.com>
2022-09-12 23:59:33 +0900

    change commit

---
+++ src/main/java/com/sw/web/WebInitializer.java
@@ -0,0 +1,25 @@
+package com.sw.web;
+
+import com.sw.web.config.SpringConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        //return null;
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        System.out.println(">>>>>>>>>> hi");
+        return new String[]{"/"};
+    }
+
+}
